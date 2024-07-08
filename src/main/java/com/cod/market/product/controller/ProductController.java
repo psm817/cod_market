828679835f9f252006f19ff14c4fe472d37ddc0b
psm817 <5004jse@gmail.com>
2024-07-08 17:47:26 +0900

    product detail view

--- src/main/java/com/cod/market/product/controller/ProductController.java
+++ src/main/java/com/cod/market/product/controller/ProductController.java
@@ -6,9 +6,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
@@ -22,5 +24,14 @@ public class ProductController {
         model.addAttribute("productList", productList);
 
         return "product/list";
+    }
+
+    @GetMapping("/detail/{id}")
+    public String list(Model model, @PathVariable("id") Long id) {
+        Optional<Product> product = productService.getProductById(id);
+
+        model.addAttribute("product", product);
+
+        return "product/detail";
     }
 }
