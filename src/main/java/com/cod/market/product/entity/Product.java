1c214c8b95ee29db598cea1aa52fb8a8b82c0996
psm817 <5004jse@gmail.com>
2024-07-10 15:08:44 +0900

    add baseEntity

--- src/main/java/com/cod/market/product/entity/Product.java
+++ src/main/java/com/cod/market/product/entity/Product.java
@@ -1,31 +1,14 @@
 package com.cod.market.product.entity;
 
+import com.cod.market.base.BaseEntity;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-
-import java.time.LocalDateTime;
-
-import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity
 @Getter
 @Setter
-public class Product {
-    @Id
-    @GeneratedValue(strategy = IDENTITY)
-    private Long id;
-
+public class Product extends BaseEntity {
     private String name;
     private int price;
-
-    @CreatedDate
-    private LocalDateTime createDate;
-
-    @LastModifiedDate
-    private LocalDateTime modifyDate;
 }
