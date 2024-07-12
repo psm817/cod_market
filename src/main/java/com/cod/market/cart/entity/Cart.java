45a540bf52f4acf906bd5eb076eef687fa9bb393
psm817 <5004jse@gmail.com>
2024-07-12 14:40:40 +0900

    add cart entity, service, repository & edit detail page

---
+++ src/main/java/com/cod/market/cart/entity/Cart.java
@@ -0,0 +1,12 @@
+package com.cod.market.cart.entity;
+
+import com.cod.market.base.BaseEntity;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Cart extends BaseEntity {
+}
