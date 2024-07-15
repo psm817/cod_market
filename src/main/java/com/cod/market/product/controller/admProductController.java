e2c8b68cf439ee9875266371e11b541d64f9c48d
psm817 <5004jse@gmail.com>
2024-07-15 16:18:37 +0900

    file upload complete

--- src/main/java/com/cod/market/product/controller/admProductController.java
+++ src/main/java/com/cod/market/product/controller/admProductController.java
@@ -1,13 +1,17 @@
 package com.cod.market.product.controller;
 
+import com.cod.market.product.entity.Product;
 import com.cod.market.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -16,7 +20,11 @@ public class admProductController {
     private final ProductService productService;
 
     @GetMapping("/create")
-    public String create() {
+    public String create(Model model) {
+        List<Product> productList = productService.getList();
+
+        model.addAttribute("productList", productList);
+
         return "adm/product/create";
     }
 
@@ -24,7 +32,7 @@ public class admProductController {
     public String createContent(@RequestParam(value = "name", defaultValue = "") String name,
                                 @RequestParam(value = "price", defaultValue = "0") int price,
                                 @RequestParam(value = "description", defaultValue = "") String description,
-                                MultipartFile thumbnail)
+                                @RequestParam(value = "thumbnail", defaultValue = "") MultipartFile thumbnail)
     {
         productService.create(name, description, price, thumbnail);
 
