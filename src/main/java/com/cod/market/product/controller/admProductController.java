1024842518fe51f20d9dc8838aa75923c3240efb
psm817 <5004jse@gmail.com>
2024-07-15 15:10:13 +0900

    add admin product content

--- src/main/java/com/cod/market/product/controller/admProductController.java
+++ src/main/java/com/cod/market/product/controller/admProductController.java
@@ -4,7 +4,9 @@ import com.cod.market.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
@@ -14,6 +16,16 @@ public class admProductController {
 
     @GetMapping("/create")
     public String create() {
+        return "adm/product/create";
+    }
+
+    @PostMapping("/create")
+    public String createContent(@RequestParam(value = "name", defaultValue = "") String name,
+                                @RequestParam(value = "price", defaultValue = "0") int price,
+                                @RequestParam(value = "description", defaultValue = "") String description)
+    {
+        productService.create(name, description, price);
+
         return "adm/product/create";
     }
 }
